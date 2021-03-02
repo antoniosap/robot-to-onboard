6ba6337ab49d260b42488951fd98d9eeccd65ebc
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-03-02 08:58:40 +0100

    two side on board / off board coding

--- base_rpi/scripts/camera.sh
+++ base_rpi/scripts/camera.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# client h264
+# vlc http://192.168.147.85:8160
+
+# server video h264
 raspivid -o - -t 0 -n -w 1920 -h 1080 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
 
