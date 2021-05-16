0c73c949a7c07eb49300aade1077ef83dae116fe
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2021-05-16 10:07:15 +0200

    ros2 joy teleop turtle success

--- base_rpi_ros2/launch/start_base.launch.py
+++ base_rpi_ros2/launch/start_base.launch.py
@@ -5,55 +5,52 @@
 # ros2 launch start_base.launch.py
 #
 import os
-from launch import LaunchDescription
+from pathlib import Path
+from launch import LaunchDescription, logging
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    logger = logging.get_logger(__name__)
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
 
-    joy_teleop_parameters_file = os.path.join('config', 'joy_teleop.yaml')
+    # params_file = os.path.join(cur_dir, '..', 'config', 'params.yaml')
+    # logger.info("params_file: " + params_file)
 
     return LaunchDescription([
-         # / joy1 / joy[sensor_msgs / msg / Joy]
-         # / joy1 / joy / set_feedback[sensor_msgs / msg / JoyFeedback]
-         # / joy_teleop1 / joy[sensor_msgs / msg / Joy]
         Node(
             package='joy',
-            namespace='joy1',
             executable='joy_node',
-            name='saitek_joy'
+            name='saitek_joy',
+            parameters=[{
+                'dev': '/dev/input/js0',
+                'deadzone': 0.3,
+                'autorepeat_rate': 20.0,
+            }]
         ),
-        #
-        DeclareLaunchArgument('teleop_config', default_value=joy_teleop_parameters_file),
-        DeclareLaunchArgument('cmd_vel', default_value='input_joy/cmd_vel'),
         Node(
-            package='joy_teleop',
-            namespace='joy_teleop_adapter',
-            executable='joy_teleop',
-            parameters=[LaunchConfiguration('teleop_config')],
+            package='teleop_twist_joy',
+            name='teleop_twist_joy_node',
+            executable='teleop_node',
+            parameters=[{
+                'axis_linear.x': 1,
+                'axis_linear.y': 0,
+                'scale_linear.x': 1.0,
+                'scale_linear.y': 1.0,
+                'axis_angular.yaw': 3,
+                'scale_angular.yaw': 1.0,
+                'enable_button': 0,
+            }],
         ),
-        #
         Node(
             package='turtlesim',
             namespace='turtlesim1',
             executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
+            name='sim',
             remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        )
+                ('/turtlesim1/turtle1/cmd_vel', '/cmd_vel'),
+            ],
+        ),
     ])
